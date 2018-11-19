2905c1ccdf8b876467c17168f1e879bb2a374151
Lionel Rowe <lionel.rowe@gmail.com>
2018-11-19 23:24:15 +0800

    add guest and host relationships

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,4 @@
 class Booking < ApplicationRecord
-  belongs_to :user
+  belongs_to :guest, class_name: 'User', foreign_key: 'user_id'
   belongs_to :garden
 end
--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,4 @@
 class Garden < ApplicationRecord
-  belongs_to :user
+  belongs_to :host, class_name: 'User', foreign_key: 'user_id'
+  has_many :bookings
 end
