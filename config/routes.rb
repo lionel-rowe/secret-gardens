aaeec4ddc2261c34470d7ad86d05602fb1000632
Lionel Rowe <lionel.rowe@gmail.com>
2018-11-19 15:19:16 +0800

    add 200 page at root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/index' => 'pages#index'
+
+  root 'pages#index'
+
 end
