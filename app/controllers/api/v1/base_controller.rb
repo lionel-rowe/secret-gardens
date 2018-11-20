48874b6790601939878e088895f47ae01809bbeb
Armani <armanivvv@gmail.com>
2018-11-20 17:34:55 +0800

    corrected pundit

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::BaseController < ActionController::Base
+  include Pundit
+  # Prevent CSRF attacks by raising an exception.    # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.    # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  after_action :verify_authorized, :except => :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, :only => :index, unless: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
 
   rescue_from StandardError,                with: :internal_server_error
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 
   def not_found(exception)
     render json: { error: exception.message }, status: :not_found
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,38 @@
 class ApplicationController < ActionController::Base
+  # include Pundit
+  # # Prevent CSRF attacks by raising an exception.    # Prevent CSRF attacks by raising an exception.
+  # # For APIs, you may want to use :null_session instead.    # For APIs, you may want to use :null_session instead.
+  # protect_from_forgery with: :exception
 
-before_action :authenticate_user!
-  include Pundit
-
-  # Pundit: white-list approach.
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-
-  # Uncomment when you *really understand* Pundit!
+  # before_action :authenticate_user!
+  # after_action :verify_authorized, :except => :index, unless: :devise_controller?
+  # after_action :verify_policy_scoped, :only => :index, unless: :devise_controller?
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  # private
+
   # def user_not_authorized
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
-
-  private
-
-  def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  end
 end
+
+# before_action :authenticate_user!
+# include Pundit
+
+# # Pundit: white-list approach.
+# after_action :verify_authorized, except: :index, unless: :skip_pundit?
+# after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+# # Uncomment when you *really understand* Pundit!
+# # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+# # def user_not_authorized
+# #   flash[:alert] = "You are not authorized to perform this action."
+# #   redirect_to(root_path)
+# # end
+
+# private
+
+# def skip_pundit?
+#   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+# end
