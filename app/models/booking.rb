30e3870e1699dda79de1c037c86cfda544a7a0d9
Armani <armanivvv@gmail.com>
2018-11-20 11:41:07 +0800

    corrected model association, added & tested seed file & added basic validaions to booking and garden

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :guest, class_name: 'User', foreign_key: 'user_id'
   belongs_to :garden
+
+  has_one :host, through: :garden
+
+  validates :start, presence: true
+  validates :end, presence: true
+  validates :use, uniqueness: true, presence: true
 end
--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,9 @@
 class Garden < ApplicationRecord
   belongs_to :host, class_name: 'User', foreign_key: 'user_id'
   has_many :bookings
+
+  validates :name, uniqueness: true, presence: true
+  validates :description, uniqueness: true, presence: true
+  validates :location, uniqueness: true, presence: true
+  validates :price, presence: true
 end
