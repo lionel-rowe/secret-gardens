44fd13d9f03699b2219e3808f979deebb09d41e6
Lionel Rowe <lionel.rowe@gmail.com>
2018-11-23 17:17:31 +0800

    merge booking page changes

--- app/javascript/packs/src/components/BookingCard.js
+++ app/javascript/packs/src/components/BookingCard.js
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import moment from 'moment-js';
 
 const styles = theme => ({
   card: {
@@ -29,17 +30,17 @@ class BookingCard extends React.Component {
     const start = this.props.booking.start;
     const end = this.props.booking.end;
     const use = this.props.booking.use;
-    const user = this.props.booking.user_id;
+    const user = this.props.booking.user_id; //TODO: fix this and show on card
     const booking = this.props.booking;
 
     return (
         <Card>
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
-              {user}: {use}
+              {use}
             </Typography>
             <Typography>
-              {start} - {end}
+              <span style={{color: '#888'}}>{moment(start).format('ll')}</span> to <span style={{color: '#888'}}>{moment(end).format('ll')}</span>
             </Typography>
           </CardContent>
         </Card>
